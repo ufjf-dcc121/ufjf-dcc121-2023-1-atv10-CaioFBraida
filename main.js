d2733a40173cdb2d0ead24cf8062cc22ff36225b
Caio Braida <caiobraida.p@gmail.com>
2023-05-30 20:21:39 -0300

    adiciona elementos no vetor

--- main.js
+++ main.js
@@ -10,11 +10,21 @@ atualiza(); // para ja ter um li assim que abre a pagina
 function envia(evento) {
     evento.preventDefault(); //n deixa formulario ser enviado
     console.log('Formulário enviado!');
-    store.estado++;
+
+    const n = form.valor.value;
+    store.estado.push(n);   //diz ele que todo array é pilha
+    form.valor.value = "";
+    form.valor.focus(); // voltaar cursor
+
     atualiza();
 }
 
 function atualiza(){
     const ol = document.querySelector('ol');
-    ol.innerHTML = `<li>${store.estado}</li>`;
+    ol.innerHTML = "";
+    for(let i = 0; i < store.estado.length; i++){
+        const li = document.createElement('li');
+        li.textContent = store.estado[i];
+        ol.appendChild(li);
+    }
 }
