05ecb79b7dbe66adc7bc2177fe3d931768378262
Caio Braida <caiobraida.p@gmail.com>
2023-05-30 19:36:36 -0300

    adiciona atualização no inicio

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const form = document.forms.entrada; //pegando forms sem queryselectror
 form.addEventListener('submit', envia);
 
 let estado = 0;
+atualiza();
 
 function envia(evento) {
     evento.preventDefault(); //n deixa formulario ser enviado
