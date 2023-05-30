d398b6c295b6160b128d9ae1738d1e020618598c
Caio Braida <caiobraida.p@gmail.com>
2023-05-30 19:56:25 -0300

    adiciona exṕortação padrão

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import { estado } from "./store.js";
+import store, {store3 } from "./store.js";
 //adicionou .js
+//defaoult n precisa estar entre chaves, se n for tem q usar {store3 }
 
 const form = document.forms.entrada; //pegando forms sem queryselectror
 form.addEventListener('submit', envia);
@@ -11,11 +12,11 @@ atualiza(); // para ja ter um li assim que abre a pagina
 function envia(evento) {
     evento.preventDefault(); //n deixa formulario ser enviado
     console.log('Formulário enviado!');
-    estado++;
+    store.estado++;
     atualiza();
 }
 
 function atualiza(){
     const ol = document.querySelector('ol');
-    ol.innerHTML = `<li>${estado}</li>`;
+    ol.innerHTML = `<li>${store.estado}</li>`;
 }
