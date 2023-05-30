5827c75d9ec007bf7b5f6c17667f10a3dc901574
Caio Braida <caiobraida.p@gmail.com>
2023-05-30 19:27:33 -0300

    adiciona um estado base

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const form = document.forms.entrada; //pegando forms sem queryselectror
 form.addEventListener('submit', envia);
 
+let estado = 0;
+
 function envia(evento) {
     evento.preventDefault(); //n deixa formulario ser enviado
     console.log('Formulário enviado!');
+    estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
 }
